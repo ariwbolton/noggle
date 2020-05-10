223fd03c5679ff8108117ddfa7b2c9ab96f24fd8
Ari Wolff Bolton <ariwbolton@gmail.com>
2020-05-10 16:10:14 -0700

    Working prefix tree

--- app/shared/lib/dictionaryPrefixTree.js
+++ app/shared/lib/dictionaryPrefixTree.js
@@ -15,8 +15,18 @@ import config from '../../config'
 function createDictionaryPrefixTrie(props) {
     const fileContents = fs.readFileSync(props.filename, 'utf8')
 
-    const lines = _.split(fileContents, '\r\n')
-    const words = _.slice(lines, 2)
+    const words = _(fileContents)
+    .split('\r\n')
+    .slice(2)
+    .value()
+
+    _.each(words, function(word) {
+        if (_.includes(word, ' ')) {
+            throw new Error(`Dictionary file malformed! Found words with spaces: ${word}`)
+        }
+    })
+
+    return trie(words)
 }
 
 const dictionaryPrefixTree = createDictionaryPrefixTrie(config.dictionaryPrefixTree)
