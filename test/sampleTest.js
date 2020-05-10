d19de95d0adf154722e7978d660f1dea650ead5e
Ari Wolff Bolton <ariwbolton@gmail.com>
2020-05-10 14:28:00 -0700

    Restructure to have frontent+backend folders

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,8 +1,5 @@
-import * as testFileValue from '../app/testFile'
-
 describe('sample test', function() {
     it('should successfully run a test', async function() {
         console.log('running!')
-        console.log(testFileValue)
     })
 })
