6ec463b0d0bf7ed3e1579e86353cc3baadea5e23
Ari Wolff Bolton <ariwbolton@gmail.com>
2020-05-10 21:00:29 -0700

    Implement NoggleCell tests

--- test/app/shared/lib/Noggle/NoggleCell.js
+++ test/app/shared/lib/Noggle/NoggleCell.js
@@ -9,51 +9,67 @@ describe('NoggleCell', function() {
         game = boardBank.getBoard()
     })
 
-    it('should get correct neighbors for a cell in the middle', function() {
-        const cell = game.getCell(1, 1)
+    function assertNeighbors(cell, expectedNeighborCharacters) {
         const neighbors = cell.neighbors
-        const expectedNeighbors = 'ABCEGIJK'
 
-        expect(neighbors.length).to.equal(expectedNeighbors.length)
+        expect(neighbors.length).to.equal(expectedNeighborCharacters.length)
         _.each(neighbors, function(neighbor) {
-            const foundCharacter = _.includes(expectedNeighbors, neighbor.character)
+            const foundCharacter = _.includes(expectedNeighborCharacters, neighbor.character)
             expect(foundCharacter).to.be.true
         })
+    }
+
+    it('should get correct neighbors for a cell in the middle', function() {
+        const cell = game.getCell(1, 1)
+        const expectedNeighborChjaracters = 'ABCEGIJK'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell on the top', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(0, 1)
+        const expectedNeighborChjaracters = 'ACEFG'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell on the left', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(1, 0)
+        const expectedNeighborChjaracters = 'ABFIJ'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell on the right', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(1, 3)
+        const expectedNeighborChjaracters = 'CDGKL'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell on the bottom', function() {
-        throw new Error('not implemented')
-    })
-
-    it('should get correct neighbors for a cell on the top', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(3, 1)
+        const expectedNeighborChjaracters = 'IJKMO'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell in the top left', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(0, 0)
+        const expectedNeighborChjaracters = 'BEF'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell in the bottom left', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(3, 0)
+        const expectedNeighborChjaracters = 'IJN'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell in the top right', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(0, 3)
+        const expectedNeighborChjaracters = 'CGH'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 
     it('should get correct neighbors for a cell in the bottom right', function() {
-        throw new Error('not implemented')
+        const cell = game.getCell(3, 3)
+        const expectedNeighborChjaracters = 'KLO'
+        assertNeighbors(cell, expectedNeighborChjaracters)
     })
 })
