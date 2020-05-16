e649e5ce5bcabf484d7392ea0ce900eae20b73cd
Ari Wolff Bolton <ariwbolton@gmail.com>
2020-05-15 17:39:12 -0700

    Begin testing with socket.io

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,7 +1,16 @@
+import path from 'path'
+import url from 'url'
+
 import Hapi from '@hapi/hapi'
+import Inert from '@hapi/inert'
+import SocketIOServer from 'socket.io'
 
 import config from '../config.js'
-import routeGamesPost from './routes/games/post.js'
+import routeGamesPost from './http/games/post.js'
+import routeIndex from './http/index.js'
+
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 /**
  *
@@ -9,10 +18,17 @@ import routeGamesPost from './routes/games/post.js'
  *
  * @returns {Promise<Hapi.Server>}
  */
-export default async function initServer() {
+export async function initHttpServer() {
     const server = new Hapi.Server({
-        port: config.backend.port
+        port: config.backend.port,
+        routes: {
+            files: {
+                relativeTo: path.join(__dirname, 'http/static')
+            }
+        }
     })
+
+    await server.register(Inert)
 
     server.ext('onPreResponse', function(request, h) {
         if (request.response.isBoom) {
@@ -28,14 +44,44 @@ export default async function initServer() {
     })
 
     // Routes
+    server.route(routeIndex)
     server.route(routeGamesPost)
 
     try {
+        console.log('Starting HTTP server...')
         await server.start()
+        console.log('Started!')
     } catch (err) {
         console.error('Problem during startup error; server should now exit', err)
         throw err
     }
 
     return server
+}
+
+export async function initWsServer(httpServer) {
+    const io = new SocketIOServer(httpServer.listener, {
+        // Engine.io options
+        transports: ['websockets']
+    })
+
+    io.on('connection', socket => {
+        console.log(`Connected! ${socket.id}`)
+
+        socket.on('disconnect', () => {
+            console.log(`Disconnected. ${socket.id}`)
+        })
+    })
+
+    return io
+}
+
+export default async function initServers() {
+    const httpServer = await initHttpServer()
+    const socketIoServer = initWsServer(httpServer)
+
+    return {
+        http: httpServer,
+        io: socketIoServer
+    }
 }
--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,4 @@
-import initServer from '../app/backend/server.js'
+import initServers from '../app/backend/server.js'
 
 // TODO: Look into top-level await
 // https://github.com/tc39/proposal-top-level-await#implementations
@@ -7,13 +7,21 @@ import initServer from '../app/backend/server.js'
 // Long-running GH issue tracking including something similar to "module.parent === ..." to ESM
 // https://github.com/nodejs/modules/issues/274
 (async function() {
-    const server = await initServer()
+    const {
+        server,
+        io
+    } = await initServers()
 
     process.on('SIGTERM', async function() {
+        console.log('Triggering WebSocket server shutdown')
+        io.close(() => console.log('WebSocket server shut down'))
+
         try {
+            console.log('Triggering HTTP server shutdown')
             await server.stop({ timeout: 10 * 1000 }) // 10 seconds
+            console.log('HTTP server shut down')
         } catch (err) {
-            console.error('An error occurred while stopping the server', err)
+            console.error('An error occurred while stopping the HTTP server', err)
             process.exit(1)
         }
     })
