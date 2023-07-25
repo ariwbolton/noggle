d3f5fbec3ba12eed4973bcf800624cd1a39ac8ce
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-07-24 17:19:15 -0700

    Get tests working

---
+++ app/shared/lib/TrieFactory.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+
+import _ from 'lodash'
+import trie from 'trie-prefix-tree'
+
+import config from '../../config.js'
+
+export class TrieFactory {
+    static fromFile(filename: string) {
+        const fileContents = fs.readFileSync(filename, 'utf8')
+
+        const words = _(fileContents)
+        .split('\r\n')
+        .slice(2)
+        .value()
+
+        _.each(words, function(word) {
+            if (_.includes(word, ' ')) {
+                throw new Error(`Dictionary file malformed! Found words with spaces: ${word}`)
+            }
+        })
+
+        return trie(words)
+    }
+}
+
+export const englishWordsTrie = TrieFactory.fromFile(config.dictionaryPrefixTree.filename)
